9e5dd48b3ca5c088181e5a3f33bccfd5af28b7d5
Rukmani <71338783+Rukmani95@users.noreply.github.com>
2021-02-19 21:00:55 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ var bg, ground;
 var rope,hero;
 var box1,box2,box3,box4,box5,box6,box7,box8,box9,box10,box11,box12,box13,box14,box15,box16,box17,box18,
 box19,box20,box21,box22,box23,box24,box25;
-var monster;
+var monster,bg;
 
 function preload() {
 bg = loadImage("images/GamingBackground.png")
